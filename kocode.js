4c141e6c364affbdd6181f3c4a4596ab496a1a7d
haruwako <61104141+haruwako@users.noreply.github.com>
2020-02-16 19:45:25 +0900

    Create kocode.js

---
+++ kocode.js
@@ -0,0 +1,56 @@
+$(window).on('load scroll', function () {
+  add_class_in_scrolling($('.svg-title1'));
+  add_class_in_scrolling($('.svg-namebar'));
+  add_class_in_scrolling($('.svg-title2'));
+  add_class_in_scrolling($('.svg-title3'));
+  add_class_in_scrolling($('.svg-title4'));
+});
+
+// スクロールで要素が表示された時にclassを付与する
+function add_class_in_scrolling(target) {
+  var winScroll = $(window).scrollTop();
+  var winHeight = $(window).height();
+  var scrollPos = winScroll + winHeight;
+
+  if (target.offset().top < scrollPos) {
+    target.addClass('start');
+
+    $('.svg-title1.start').attr('src', '../img/aboutme_animated.svg');
+    $('.svg-namebar.start').attr('src', '../img/namebar_animated.svg');
+    $('.svg-title2.start').attr('src', '../img/service_animated.svg');
+    $('.svg-title3.start').attr('src', '../img/works_animated.svg');
+    $('.svg-title4.start').attr('src', '../img/contact_animated.svg');
+  }
+
+}
+
+$(window).on('scroll', function () {
+
+  var elem = $('.elem');
+  var isAnimate = 'isAnimate';
+
+  elem.each(function () {
+
+    var elemOffset = $(this).offset().top;
+    var scrollPos = $(window).scrollTop();
+    var wh = $(window).height();
+
+    if (scrollPos > elemOffset - wh) {
+      $(this).addClass(isAnimate);
+    }
+  });
+});
+
+
+$(function () {
+  $("a[href^='#']").click(function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top;
+    var speed = 500; // scroll speed（mms）
+    $("body,html").animate({
+      scrollTop: position
+    }, speed, "swing"); // scroll way
+    return false;
+  });
+});
