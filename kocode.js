83a23e6413a57fbf9b88869adc5ab84d8aee556b
haruwako <61104141+haruwako@users.noreply.github.com>
2020-02-18 14:20:59 +0900

    Update kocode.js

--- kocode.js
+++ kocode.js
@@ -46,7 +46,7 @@ $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     var wHeight = $(window).height();
 
-    if (scroll > hit - wHeight - wHeight / 100) {
+    if (scroll > hit - wHeight - wHeight / 30) {
       $(this).addClass("isAnimate");
     }
   });
